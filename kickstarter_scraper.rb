78fc98fcd11f07fe08244460d80dc5d80bef5285
Christian Polanco <chrispolanco@sbcglobal.net>
2019-07-29 01:11:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,10 @@
 require 'nokogiri' 
 
-def create_project_hash
+  def create_project_hash
   
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
+    html = File.read('fixtures/kickstarter.html')
+    kickstarter = Nokogiri::HTML(html)
+  end 
   
   
 end
